bd1809c37157bd0fa52ac0f14f6196fd0b323710
Ilya4rh <chernykn12ilya12@gmail.com>
2024-11-12 19:21:36 +0500

    Реализация добавления первого прямоугольника

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -2,12 +2,22 @@
 
 namespace TagsCloudVisualization;
 
-public class CircularCloudLayouter
+public class CircularCloudLayouter : ICircularCloudLayouter
 {
     private readonly Point center;
+    private readonly List<Rectangle> addedRectangles;
     
     public CircularCloudLayouter(Point center)
     {
         this.center = center;
+        addedRectangles = new List<Rectangle>();
+    }
+
+    public Rectangle PutNextRectangle(Size rectangleSize)
+    {
+        if (addedRectangles.Count == 0)
+            return new Rectangle(center, rectangleSize);
+
+        return new Rectangle(new Point(center.X + 1, center.Y + 2), rectangleSize);
     }
 }
--- cs/Tests/CircularCloudLayouterTests.cs
+++ cs/Tests/CircularCloudLayouterTests.cs
@@ -1,8 +1,24 @@
-﻿using NUnit.Framework;
+﻿using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudVisualization;
 
 namespace Tests;
 
 [TestFixture]
 public class CircularCloudLayouterTests
 {
+    [TestCase(0, 0, 10, 15)]
+    [TestCase(2, 7, 5, 9)]
+    public void PutNextRectangle_ShouldRectangleInCenter_WhenAddFirstRectangle(int centerX, int centerY, 
+        int rectangleWidth, int rectangleHeight)
+    {
+        var center = new Point(centerX, centerY);
+        var c = new CircularCloudLayouter(center);
+        var rectangleSize = new Size(rectangleWidth, rectangleHeight);
+
+        var addedRectangle = c.PutNextRectangle(rectangleSize);
+
+        addedRectangle.Location.Should().BeEquivalentTo(center);
+    }
 }
