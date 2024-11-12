c3361d06bb2f352879e5bbc11e0e1d2c39a1691d
Ilya4rh <chernykn12ilya12@gmail.com>
2024-11-12 18:06:50 +0500

    Add ICircularCloudLayouter.cs, CircularCloudLayouter.cs

---
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+public class CircularCloudLayouter
+{
+    private readonly Point center;
+    
+    public CircularCloudLayouter(Point center)
+    {
+        this.center = center;
+    }
+}
