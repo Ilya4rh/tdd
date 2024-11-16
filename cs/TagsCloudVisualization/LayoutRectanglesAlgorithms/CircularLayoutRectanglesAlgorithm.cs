5c9d9b7bb8bdef1a9195738e7315437e6916833a
Ilya4rh <chernykn12ilya12@gmail.com>
2024-11-16 14:47:01 +0500

    Перенес реализацию раскладки по кругу в новый класс

--- cs/TagsCloudVisualization/LayoutRectanglesAlgorithms/CircularLayoutRectanglesAlgorithm.cs
+++ cs/TagsCloudVisualization/LayoutRectanglesAlgorithms/CircularLayoutRectanglesAlgorithm.cs
@@ -1,6 +1,43 @@
-﻿namespace TagsCloudVisualization.LayoutRectanglesAlgorithms;
+﻿using System.Drawing;
 
-public class CircularLayoutRectanglesAlgorithm
+namespace TagsCloudVisualization.LayoutRectanglesAlgorithms;
+
+public class CircularLayoutRectanglesAlgorithm : ILayoutRectanglesAlgorithm
 {
+    private readonly List<Rectangle> addedRectangles = [];
+    private double currentAngleOfCircle;
+    private double currentRadiusOfCircle;
+    private const double OneDegree = Math.PI / 180;
+    private const double FullCircleRotation = 2 * Math.PI;
     
+    public Rectangle PutNextRectangle(Size rectangleSize, Point cloudCenter)
+    {
+        Rectangle rectangle;
+
+        do
+        {
+            var x = cloudCenter.X + (int)(currentRadiusOfCircle * Math.Cos(currentAngleOfCircle)) - rectangleSize.Width / 2;
+            var y = cloudCenter.Y + (int)(currentRadiusOfCircle * Math.Sin(currentAngleOfCircle)) - rectangleSize.Height / 2;
+            rectangle = new Rectangle(new Point(x, y), rectangleSize);
+
+            // увеличиваем угол на 1 градус
+            currentAngleOfCircle += OneDegree; 
+            
+            // проверяем не прошли ли целый круг
+            if (currentAngleOfCircle > FullCircleRotation)
+            {
+                currentAngleOfCircle = 0;
+                currentRadiusOfCircle++;
+            }
+        } while (IntersectWithAddedRectangles(rectangle));
+        
+        addedRectangles.Add(rectangle);
+        
+        return rectangle;
+    }
+    
+    private bool IntersectWithAddedRectangles(Rectangle rectangle)
+    {
+        return addedRectangles.Any(addedRectangle => addedRectangle.IntersectsWith(rectangle));
+    }
 }
--- cs/TagsCloudVisualization/LayoutRectanglesAlgorithms/ILayoutRectanglesAlgorithm.cs
+++ cs/TagsCloudVisualization/LayoutRectanglesAlgorithms/ILayoutRectanglesAlgorithm.cs
@@ -4,5 +4,5 @@ namespace TagsCloudVisualization.LayoutRectanglesAlgorithms;
 
 public interface ILayoutRectanglesAlgorithm
 {
-    Rectangle PutNextRectangle(Size rectangleSize, int center);
+    Rectangle PutNextRectangle(Size rectangleSize, Point cloudCenter);
 }
