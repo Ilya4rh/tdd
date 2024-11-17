61330d315e6636e8ac94d08fec354f5ffae6dcd7
Ilya4rh <chernykn12ilya12@gmail.com>
2024-11-17 23:37:22 +0500

    Изменил тесты расстановки прямоугольников, добавил класс с утилитами

--- cs/TagsCloudVisiualizationTests/CircularCloudLayoutTests.cs
+++ cs/TagsCloudVisiualizationTests/CircularCloudLayoutTests.cs
@@ -3,13 +3,14 @@ using System.Drawing.Imaging;
 using FluentAssertions;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
+using TagsCloudVisiualizationTests.Utils;
 using TagsCloudVisualization;
 using TagsCloudVisualization.LayoutRectanglesInCloudAlgorithms;
 
-namespace CircularCloudLayouterTests;
+namespace TagsCloudVisiualizationTests;
 
 [TestFixture]
-public class CircularLayoutRectanglesInCloudAlgorithmTests
+public class CircularCloudLayoutTests
 {
     private ICircularCloudLayouter cloudLayouter;
     private List<Rectangle> addedRectangles;
@@ -17,7 +18,8 @@ public class CircularLayoutRectanglesInCloudAlgorithmTests
     [SetUp]
     public void Setup()
     {
-        cloudLayouter = new CircularCloudLayouter(new Point(0, 0), new CircularLayoutAlgorithm());
+        var center = new Point(0, 0);
+        cloudLayouter = new CircularCloudLayouter(center, new CircularLayoutAlgorithm(center));
         addedRectangles = [];
     }
 
@@ -41,17 +43,6 @@ public class CircularLayoutRectanglesInCloudAlgorithmTests
         VisualizationCircularCloudLayout.SaveLayout(bitmap, pathImageStored, $"{testName}.png", ImageFormat.Png);
         
         Console.WriteLine($@"Tag cloud visualization saved to file {pathImageStored}\{testName}.png");
-    }
-    
-    [Test]
-    public void PutNextRectangle_ShouldRectangleInCenter_WhenAddFirstRectangle()
-    {
-        var rectangleSize = new Size(10, 15);
-
-        var addedRectangle = cloudLayouter.PutNextRectangle(rectangleSize);
-        var expectedRectangleStartPoint = new Point(-addedRectangle.Width / 2, - addedRectangle.Height / 2);
-
-        addedRectangle.Location.Should().BeEquivalentTo(expectedRectangleStartPoint);
     }
     
     [TestCase(10, 5, 15)]
@@ -85,12 +76,14 @@ public class CircularLayoutRectanglesInCloudAlgorithmTests
         addedRectangles.AddRange(rectangleSizes.Select(t => cloudLayouter.PutNextRectangle(t)));
 
         var distances = addedRectangles
-            .Select(rectangle => CalculateDistanceBetweenCenterRectangleAndCenterCloud(rectangle, new Point(0, 0)))
+            .Select(rectangle => 
+                GeometryUtils.CalculateDistanceBetweenCenterRectangleAndCenterCloud(rectangle, new Point(0, 0)))
             .ToArray();
 
         for (var i = 1; i < distances.Length; i++)
         {
-            var distanceBetweenRectangles = CalculateDistanceBetweenRectangles(addedRectangles[i], addedRectangles[i - 1]);
+            var distanceBetweenRectangles =
+                GeometryUtils.CalculateDistanceBetweenRectangles(addedRectangles[i], addedRectangles[i - 1]);
             distances[i].Should().BeApproximately(distances[i - 1], distanceBetweenRectangles);
         }
     }
@@ -108,25 +101,5 @@ public class CircularLayoutRectanglesInCloudAlgorithmTests
             .ToList();
 
         return generatedSizes;
-    }
-
-    private static double CalculateDistanceBetweenCenterRectangleAndCenterCloud(Rectangle rectangle, Point center)
-    {
-        var centerRectangle = new Point(rectangle.X + rectangle.Width / 2, rectangle.Y + rectangle.Height / 2);
-        
-        return CalculateDistanceBetweenPoints(centerRectangle, center);
-    }
-
-    private static double CalculateDistanceBetweenRectangles(Rectangle rectangle1, Rectangle rectangle2)
-    {
-        var centerRectangle1 = new Point(rectangle1.X + rectangle1.Width / 2, rectangle1.Y + rectangle1.Height / 2);
-        var centerRectangle2 = new Point(rectangle2.X + rectangle2.Width / 2, rectangle2.Y + rectangle2.Height / 2);
-
-        return CalculateDistanceBetweenPoints(centerRectangle1, centerRectangle2);
-    }
-
-    private static double CalculateDistanceBetweenPoints(Point point1, Point point2)
-    {
-        return Math.Sqrt(Math.Pow(point1.X - point2.X, 2) + Math.Pow(point1.Y - point2.Y, 2));
     }
 }
---
+++ cs/TagsCloudVisiualizationTests/Utils/GeometryUtils.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisiualizationTests.Utils;
+
+public static class GeometryUtils
+{
+    public static double CalculateDistanceBetweenCenterRectangleAndCenterCloud(Rectangle rectangle, Point center)
+    {
+        var centerRectangle = new Point(rectangle.X + rectangle.Width / 2, rectangle.Y + rectangle.Height / 2);
+        
+        return CalculateDistanceBetweenPoints(centerRectangle, center);
+    }
+
+    public static double CalculateDistanceBetweenRectangles(Rectangle rectangle1, Rectangle rectangle2)
+    {
+        var centerRectangle1 = new Point(rectangle1.X + rectangle1.Width / 2, rectangle1.Y + rectangle1.Height / 2);
+        var centerRectangle2 = new Point(rectangle2.X + rectangle2.Width / 2, rectangle2.Y + rectangle2.Height / 2);
+
+        return CalculateDistanceBetweenPoints(centerRectangle1, centerRectangle2);
+    }
+
+    public static double CalculateDistanceBetweenPoints(Point point1, Point point2)
+    {
+        return Math.Sqrt(Math.Pow(point1.X - point2.X, 2) + Math.Pow(point1.Y - point2.Y, 2));
+    }
+}
